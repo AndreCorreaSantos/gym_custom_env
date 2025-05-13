a004e6af23b6709e9eaa7d4d0d7d9c7be07daa19
AndreCorreaSantos <acorreasantos2002@gmail.com>
2025-05-13 20:13:34 -0300

    adding new files

--- run_grid_trail_v0.py
+++ run_grid_trail_v0.py
@@ -2,26 +2,28 @@ from pettingzoo.utils import parallel_to_aec
 from gymnasium_env.grid_trail import GridTrailParallelEnv 
 import numpy as np
 
-env = GridTrailParallelEnv(render_mode="human", size=40,num_agents=1)
-env.reset(seed=42)
+env = GridTrailParallelEnv(render_mode="human", size=40,num_agents=50)
+env.reset()
+
+
 
 done = {agent: False for agent in env.agents}
 steps = 0
-MAX_TIMESTEPS = 500
+MAX_TIMESTEPS = 1000
 
-while not all(done.values()) and steps < MAX_TIMESTEPS:
+while not any(done.values()) and steps < MAX_TIMESTEPS:
     actions = {
-        agent: 0
+        agent: env.action_space(agent).sample()
         for agent in env.agents
         if not done[agent]
     }
     observations, rewards, terminations, truncations, infos = env.step(actions)
+    print(f"Step: {steps}")
+    print(observations)
     env.render()
-    print("Observations agent_0:")
-    print(observations["agent_0"])
-    print(f"\nStep: {steps}")
-    for agent in env.agents:
-        print(f"{agent}: Action: {actions.get(agent)}, Reward: {rewards[agent]}, Done: {terminations[agent] or truncations[agent]}")
+    # print(f"\nStep: {steps}")
+    # for agent in env.agents:
+        # print(f"{agent}: Action: {actions.get(agent)}, Reward: {rewards[agent]}, Done: {terminations[agent] or truncations[agent]}")
 
     done = {agent: terminations[agent] or truncations[agent] for agent in env.agents}
     steps += 1
